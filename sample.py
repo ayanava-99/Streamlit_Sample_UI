dd6f888f1e4c5ad5eebf73cb6c583d29768de82c
Ayanava Dutta <ayanava1999@gmail.com>
2020-12-12 23:09:08 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -13,7 +13,7 @@ option = st.sidebar.selectbox(
      ['Login','StreamLit Documentation'])
 
 if option=='Login':
-    name=st.text_input("Enter Your Name","abcd",max_chars=20)
+    name=st.text_input("Enter Your Name",max_chars=20)
     selection = st.radio("Gender",("Male","Female"))
     col1,col2=st.beta_columns(2)
     age=col1.slider("Enter Your Age",max_value=80,min_value=10,value=21)
@@ -27,6 +27,8 @@ if option=='Login':
     if st.button(label="Submit"):
         st.success("Your form has been submitted Sucessfully")
         st.balloons()
+        st.text("You have entered:")
+        st.text("Name: "+str(name)+'\n\n'+"Age: "+str(age)+'\n\n'+"Date of Birth: "+str(dateofbirth))
 if option=='StreamLit Documentation':
     st.markdown("""<a href="https://docs.streamlit.io/en/stable/">StreamLit Documentation</a>""", unsafe_allow_html=True,)
     
